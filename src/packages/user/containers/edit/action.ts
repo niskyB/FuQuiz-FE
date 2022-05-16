f801b5580cc91eeef3b4ec35a690295f35e6efc0
Duc Dauuu <dauleduc2@gmail.com>
2022-05-17 00:38:45 +0700

    UPDATE - update avatar

--- src/packages/user/containers/edit/action.ts
+++ src/packages/user/containers/edit/action.ts
@@ -2,36 +2,24 @@ import { User } from '../../../../core/models/user';
 import { http } from '../../../../core/api';
 
 export interface UpdateUserDto extends Pick<User, 'fullName' | 'gender' | 'mobile' | 'email'> {
-    image: FileList;
+    image: File;
 }
 
 export const updateUser = async (data: UpdateUserDto) => {
     let { email, ...other } = data;
     let form = new FormData();
+
     for (const key in other) {
         if (Object.prototype.hasOwnProperty.call(data, key)) {
             const element = (data as any)[key];
             form.append(key, element);
         }
-
-        if (key === 'image') {
-            let element = (data as any)[key];
-            if (element.length <= 0) {
-                form.append(key, '');
-            } else {
-                form.append(key, element[0]);
-                console.log(element[0]);
-            }
-        }
     }
-    const image = form.getAll('image');
-    // console.log(image);
-
     const res = await http.put('/user', form, {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
     });
-
+    console.log(res);
     return res.data;
 };
