fdca22f4471cf29c785533447bcc832bd3317635
Kaine <kainesv86@gmail.com>
2022-05-19 12:29:06 +0700

    Add update slider

---
+++ pages/dashboard/slider/edit/[id].tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Component } from 'react';
+import { RouterProtectionWrapper } from '../../../../src/core/components/routerProtection';
+import { UserRole } from '../../../../src/core/models/role';
+import DashBoardLayout from '../../../../src/packages/dashboard/components/dashboardLayout';
+import AddSlider from '../../../../src/packages/dashboard/containers/addSlider';
+
+interface EditSliderProps {}
+
+const EditSlider: React.FunctionComponent<EditSliderProps> = () => {
+    return (
+        <RouterProtectionWrapper acceptRoles={[UserRole.ADMIN, UserRole.MARKETING]}>
+            <DashBoardLayout>
+                <AddSlider />
+            </DashBoardLayout>
+        </RouterProtectionWrapper>
+    );
+};
+
+export default EditSlider;
