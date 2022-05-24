133562b1f96df094c840dee0d19dd4b34f82ed9e
Duc Dau <dauleduc2@gmail.com>
2022-05-24 16:56:02 +0700

    REFACTOR - file func

---
+++ src/packages/slider/containers/editSlider/hooks.ts
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Slider } from '../../../../core/models/slider';
+import { getSliderById } from './action';
+
+interface useGetSliderProps {
+    id: string;
+}
+
+export const useGetSlider = ({ id }: useGetSliderProps) => {
+    const [slider, setSlider] = React.useState<Slider>();
+    const [imageUrl, setImageUrl] = React.useState<string>('');
+
+    React.useEffect(() => {
+        getSliderById(id).then((data) => {
+            setImageUrl(data.imageUrl);
+            setSlider(data);
+        });
+    }, [id]);
+
+    return { slider, imageUrl, setImageUrl };
+};
