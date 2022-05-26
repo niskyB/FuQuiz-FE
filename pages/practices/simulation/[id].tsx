92bbe8b1136c5d7531d97bb4d3677322a02df4fd
Kaine <kainesv86@gmail.com>
2022-05-26 17:29:34 +0700

    Add - Simulation detail

--- pages/practices/simulation/[id].tsx
+++ pages/practices/simulation/[id].tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { RouterProtectionWrapper } from '../../../src/core/components/routerProtection';
 import { AllRole } from '../../../src/core/models/user';
 import SimulationDetail from '../../../src/packages/simulation/containers/simulationDetail';
-import SimulationList from '../../../src/packages/simulation/containers/simulationList';
 import { StoreLayout } from '../../../src/packages/store';
 
 interface SimulationDetailPageProps {}
