97a4c55355a2c0bd7c3865f1a9348a71251ec992
unknown <kainesv86@gmail.com>
2022-05-27 07:44:34 +0700

    Add - Review practice

---
+++ src/packages/practices/containers/reviewPractice/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface ReviewPracticeProps {}
+
+const ReviewPractice: React.FunctionComponent<ReviewPracticeProps> = () => {
+    return <div></div>;
+};
+
+export default ReviewPractice;
