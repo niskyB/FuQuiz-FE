bfe0b07396da876e8f7b2db8f531e7104c82c0ed
Kaine <kainesv86@gmail.com>
2022-05-31 17:26:24 +0700

    Add - get list type T

--- src/packages/users/containers/userList/hook.ts
+++ src/packages/users/containers/userList/hook.ts
@@ -2,9 +2,13 @@ import * as React from 'react';
 import { User } from '../../../../core/models/user';
 
 export const useGetUserList = () => {
-    const [userList, setUserList] = React.useState<User[]>();
+    const [userList, setUserList] = React.useState<User[]>([]);
     const [count, setCount] = React.useState<number>(0);
     React.useEffect(() => {}, []);
 
     return { count, userList };
+};
+
+export const useGetList = <T>(url: string) => {
+    const [list, setList] = React.useState<T[]>([]);
 };
