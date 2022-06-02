17d8fbf7ab5d7fd1e4e195eb4cec27e2be3bba42
Kaine <kainesv86@gmail.com>
2022-06-02 13:45:09 +0700

    Fix - Table slider

---
+++ src/core/common/dataField/type.ts
@@ -0,0 +1,11 @@
+// import { SelectionFieldValues } from '../interface';
+// export enum TypeFieldData {
+//     BLOG_CATEGORY = 'blogCategory',
+//     BLOG_CATEGORY = 'subjectCategory',
+
+// }
+
+// export const statusFieldData: SelectionFieldValues<TypeFieldData>[] = [
+//     { label: 'Active', value: '' },
+//     { label: 'Inactive', value: false },
+// ];
---
+++ src/packages/blogCategory/containers/blogCategoryList/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface BlogCategoryListProps {}
+
+const BlogCategoryList: React.FunctionComponent<BlogCategoryListProps> = () => {
+    return <div></div>;
+};
+
+export default BlogCategoryList;
