471aef482cb4fedd7c19822a5fc4525a1261cc64
unknown <kainesv86@gmail.com>
2022-06-11 11:48:15 +0700

    Fix error html

--- src/packages/blog/container/sideBlog/index.tsx
+++ src/packages/blog/container/sideBlog/index.tsx
@@ -11,7 +11,7 @@ export const SideBlog: React.FunctionComponent<BlogListProps> = ({ blogList }) =
         <div className="flex flex-col space-y-5 divide-y-2">
             <h2 className="text-2xl font-semibold">Latest post</h2>
             {blogList.map((blog) => (
-                <Link href={`${routes.blogUrl}/${blog.id}`} passHref>
+                <Link key={blog.id} href={`${routes.blogUrl}/${blog.id}`} passHref>
                     <div className="flex p-3 space-x-3 transition-all duration-300 bg-white rounded-md cursor-pointer h-36 hover:scale-110">
                         <img className="object-cover w-1/2 " src={blog.thumbnailUrl} />
                         <div className="">
