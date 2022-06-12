8c69301865e7aa03eb2997d245f12b6522ed70cd
unknown <kainesv86@gmail.com>
2022-06-12 12:34:01 +0700

    Fix title to name

--- src/packages/subject/container/addSubject/index.tsx
+++ src/packages/subject/container/addSubject/index.tsx
@@ -68,8 +68,8 @@ export const AddSubject: React.FunctionComponent<AddSubjectProps> = () => {
 
                         <div className="w-full mt-6 space-y-6 sm:max-w-3xl sm:mt-5 sm:space-y-5">
                             <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                <label htmlFor="title" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                    Title
+                                <label htmlFor="name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                    Name
                                 </label>
                                 <div className="mt-1 sm:mt-0 sm:col-span-2">
                                     <TextField label="" name="name" type="text" />
