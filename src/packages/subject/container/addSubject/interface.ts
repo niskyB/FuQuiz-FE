6c1cef8d1afc0ad1366b4a60075bb8d3cfbb5026
Duc Dauuu <dauleduc2@gmail.com>
2022-06-12 22:04:37 +0700

    ADD - add new blog

--- src/packages/subject/container/addSubject/interface.ts
+++ src/packages/subject/container/addSubject/interface.ts
@@ -1,9 +1,8 @@
 import { Subject } from '../../../../core/models/subject';
 
-export interface AddSubjectFormDTO extends Pick<Subject, 'name' | 'tagLine' | 'description'> {
+export interface AddSubjectFormDTO extends Pick<Subject, 'name' | 'tagLine' | 'description' | 'isFeature'> {
     category: string;
     assignTo: string;
     image: File | null;
-    isFeature: boolean | '';
     isActive: boolean | '';
 }
