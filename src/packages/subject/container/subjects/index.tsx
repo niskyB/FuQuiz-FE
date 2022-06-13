508e58203ffc5c8079e8239f7c5bfd73a84b3b32
Duc Dauuu <dauleduc2@gmail.com>
2022-06-13 15:52:11 +0700

    ADD - register button

--- src/packages/subject/container/subjects/index.tsx
+++ src/packages/subject/container/subjects/index.tsx
@@ -8,9 +8,12 @@ import { BlogListFilterDTO } from './interface';
 import { SideBox } from '../../../store/components/sideBox';
 import Contact from '../../../store/container/Contact';
 import { UserFilter } from '../../components/userFilter';
+
+/* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/solid';
+
 interface SubjectsProps extends BlogListFilterDTO {}
 
 function classNames(...classes: any) {
@@ -53,7 +56,7 @@ export const Subjects: React.FunctionComponent<SubjectsProps> = ({ category, cur
                 <div className="flex flex-col space-y-5">
                     <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
                         {subjects.map((item) => (
-                            <div key={item.id} className="flex flex-col w-full py-5 overflow-hidden bg-white rounded-lg shadow-lg ">
+                            <div key={item.id} className="flex flex-col w-full py-5 bg-white rounded-lg shadow-lg ">
                                 <Link href={`${routes.subjectUrl}/${item.id}`} passHref>
                                     <div className="min-w-full mx-auto bg-white cursor-pointer">
                                         <img className="object-cover h-48 py-3 mx-auto" src={item.thumbnailUrl} alt="thumbnail" />
@@ -78,12 +81,57 @@ export const Subjects: React.FunctionComponent<SubjectsProps> = ({ category, cur
                                 </div>
 
                                 <div className="w-full px-5">
-                                    <button
-                                        type="button"
-                                        className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    >
-                                        Register now
-                                    </button>
+                                    <Menu as="div" className="relative inline-block w-full text-left">
+                                        <div className="w-full">
+                                            <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                                                <div className="">Buy now</div>
+                                                <ChevronDownIcon className="w-5 h-5 ml-2 -mr-1" aria-hidden="true" />
+                                            </Menu.Button>
+                                        </div>
+
+                                        <Transition
+                                            as={Fragment}
+                                            enter="transition ease-out duration-100"
+                                            enterFrom="transform opacity-0 scale-95"
+                                            enterTo="transform opacity-100 scale-100"
+                                            leave="transition ease-in duration-75"
+                                            leaveFrom="transform opacity-100 scale-100"
+                                            leaveTo="transform opacity-0 scale-95"
+                                        >
+                                            <Menu.Items className="absolute right-0 w-full mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                <div className="py-1">
+                                                    <Menu.Item>
+                                                        {({ active }) => (
+                                                            <a
+                                                                href="#"
+                                                                className={classNames(
+                                                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                    'flex px-4 py-2 text-sm justify-between'
+                                                                )}
+                                                            >
+                                                                <div className="font-semibold">Standard</div>
+                                                                <div className="">30.000 đ/ 3 months</div>
+                                                            </a>
+                                                        )}
+                                                    </Menu.Item>
+                                                    <Menu.Item>
+                                                        {({ active }) => (
+                                                            <a
+                                                                href="#"
+                                                                className={classNames(
+                                                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                    'flex px-4 py-2 text-sm justify-between'
+                                                                )}
+                                                            >
+                                                                <div className="font-semibold">Premium</div>
+                                                                <div className="">80.000 đ/ 9 months</div>
+                                                            </a>
+                                                        )}
+                                                    </Menu.Item>
+                                                </div>
+                                            </Menu.Items>
+                                        </Transition>
+                                    </Menu>
                                 </div>
                             </div>
                         ))}
