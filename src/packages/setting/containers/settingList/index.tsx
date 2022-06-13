3ea2117755cef10b3765e265fda8d3f5ecd87878
loc <hoanglocst900@gmail.com>
2022-06-13 23:41:01 +0700

    ADD - setting ui

--- src/packages/setting/containers/settingList/index.tsx
+++ src/packages/setting/containers/settingList/index.tsx
@@ -22,30 +22,37 @@ export const SettingList: React.FunctionComponent<SettingListProps> = ({ current
     const [settings, setSettings] = React.useState<Setting[]>([
         {
             id: '12312-11-231',
-            type: { id: 'hzdio-11-asd', name: 'Type 1' },
-            value: 'Setting 1',
+            type: { id: 'hzdio-11-asd', name: 'Role' },
+            value: 'cfd80d32-eb1b-11ec-8fea-0242ac120002',
             order: 1,
             isActivate: true,
         },
         {
             id: '12312-11-231',
-            type: { id: 'hzdio-11-asd', name: 'Type 1' },
-            value: 'Setting 1',
-            order: 1,
+            type: { id: 'hzdio-11-asd', name: 'Role' },
+            value: 'ef43be00-eb1b-11ec-8fea-0242ac120002',
+            order: 2,
             isActivate: true,
         },
         {
             id: '12312-11-231',
-            type: { id: 'hzdio-11-asd', name: 'Type 1' },
-            value: 'Setting 1',
-            order: 1,
+            type: { id: 'hzdio-11-asd', name: 'Role' },
+            value: 'f296615c-eb1b-11ec-8fea-0242ac120002',
+            order: 3,
             isActivate: true,
         },
         {
             id: '12312-11-231',
-            type: { id: 'hzdio-11-asd', name: 'Type 1' },
-            value: 'Setting 1',
-            order: 1,
+            type: { id: 'hzdio-11-asd', name: 'Role' },
+            value: 'f5aa34a4-eb1b-11ec-8fea-0242ac120002',
+            order: 4,
+            isActivate: true,
+        },
+        {
+            id: '12312-11-231',
+            type: { id: 'hzdio-11-asd', name: 'Role' },
+            value: 'f8deb802-eb1b-11ec-8fea-0242ac120002',
+            order: 5,
             isActivate: true,
         },
     ]);
@@ -59,9 +66,7 @@ export const SettingList: React.FunctionComponent<SettingListProps> = ({ current
             <div className="sm:flex sm:items-center">
                 <div className="sm:flex-auto">
                     <h1 className="text-xl font-semibold text-gray-900">Settings</h1>
-                    <p className="mt-2 text-sm text-gray-700">
-                        A list of all settings in database including their Information, create date and active status.
-                    </p>
+                    <p className="mt-2 text-sm text-gray-700">A list of settings including their id, type, value, order, status.</p>
                 </div>
                 <div className="mt-4 space-x-2 sm:mt-0 sm:ml-16 sm:flex-none">
                     <Link href={routes.addSettingUrl} passHref>
@@ -75,16 +80,22 @@ export const SettingList: React.FunctionComponent<SettingListProps> = ({ current
                 <FormWrapper methods={methods}>
                     <form className="space-y-4" onSubmit={methods.handleSubmit(_handleOnSubmit)}>
                         <div className="flex space-x-4">
-                            <TextField require={false} name="value" label="value" />
                             <SelectField
                                 label="Type"
                                 values={[
-                                    { label: 'Type 1', value: '1' },
-                                    { label: 'Type 2', value: '2' },
+                                    { label: 'Role', value: 'ASC' },
+                                    { label: 'System Menu', value: 'DESC' },
+                                    { label: 'Blog Categories', value: 'DESC' },
+                                    { label: 'Subject Categories', value: 'DESC' },
+                                    { label: 'Test Types ', value: 'DESC' },
+                                    { label: 'Question Levels ', value: 'DESC' },
+                                    { label: 'Lesson Types ', value: 'DESC' },
+                                    { label: 'Subject Dimension', value: 'DESC' },
                                 ]}
                                 require={false}
-                                name="type"
+                                name="orderBy"
                             />
+                            <TextField require={false} name="value" label="value" />
                             <SelectField
                                 label="Active"
                                 values={[
@@ -93,6 +104,27 @@ export const SettingList: React.FunctionComponent<SettingListProps> = ({ current
                                 ]}
                                 require={false}
                                 name="isActive"
+                            />
+                            <SelectField
+                                label="Order By"
+                                values={[
+                                    { label: 'Id', value: 'ASC' },
+                                    { label: 'Type', value: 'DESC' },
+                                    { label: 'Value', value: 'DESC' },
+                                    { label: 'Order', value: 'DESC' },
+                                    { label: 'Status ', value: 'DESC' },
+                                ]}
+                                require={false}
+                                name="orderBy"
+                            />
+                            <SelectField
+                                label="Sort"
+                                values={[
+                                    { label: 'ASCENDING', value: 'ASC' },
+                                    { label: 'DESCENDING', value: 'DESC' },
+                                ]}
+                                require={false}
+                                name="order"
                             />
                         </div>
                         <div className="flex justify-end">
@@ -147,9 +179,9 @@ export const SettingList: React.FunctionComponent<SettingListProps> = ({ current
                                                 </TableDescription>
                                                 <TableDescription>
                                                     {setting.isActivate ? (
-                                                        <button className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-red-400 border border-transparent rounded-md shadow-sm cursor-pointer hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2">
-                                                            Deactivate
-                                                        </button>
+                                                        <Link href={`${routes.editSettingUrl}/${setting.id}`} passHref>
+                                                            <p className="text-red-600 cursor-pointer hover:text-red-900">Deactivate</p>
+                                                        </Link>
                                                     ) : (
                                                         <button className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm cursor-pointer hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                                                             Activate
