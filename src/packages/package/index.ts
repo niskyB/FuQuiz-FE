92434a6e12485996339e39e88e7d62c7acae9420
unknown <kainesv86@gmail.com>
2022-06-19 04:01:37 +0700

    Fix package ui

--- src/packages/package/index.ts
+++ src/packages/package/index.ts
@@ -1,3 +1,6 @@
 export * from './containers/addPackage';
 export * from './containers/packageList';
 export * from './containers/editPackage';
+
+export * from './common/hooks/useGetPricePackageBySubjectId';
+export * from './common/hooks/useGetPricePackageListBySubjectId';
