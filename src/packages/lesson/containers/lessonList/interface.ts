f2fcd27ea50502b74ab320f4bb3e45572b5bf090
Duc Dauuu <dauleduc2@gmail.com>
2022-06-21 03:27:28 +0700

    update - lesson

--- src/packages/lesson/containers/lessonList/interface.ts
+++ src/packages/lesson/containers/lessonList/interface.ts
@@ -1,5 +1,10 @@
 export interface FilterLessonListDTO {
     id: string;
+    title: string;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+    isActive: string;
 }
 
 export interface UpdateLessonActivationDTO {
