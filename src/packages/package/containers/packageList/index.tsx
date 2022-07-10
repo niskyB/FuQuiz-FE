fdb99bce1c27ac0f73c9eb4723ed9c2d9d048815
BuildTools <kainesv86@gmail.com>
2022-07-10 12:03:48 +0700

    Fix auth permission

--- src/packages/package/containers/packageList/index.tsx
+++ src/packages/package/containers/packageList/index.tsx
@@ -3,15 +3,18 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import { Table, TableDescription, TableHead, TableRow } from '../../../../core/components/table';
 import { TableBody } from '../../../../core/components/table/tableBody';
+import { UserRole } from '../../../../core/models/role';
 import { routes } from '../../../../core/routes';
+import { useStoreUser } from '../../../../core/store';
 import { useGetPricePackageListBySubjectId } from '../../common/hooks/useGetPricePackageListBySubjectId';
 
 interface PackageListProps {
     subjectId: string;
 }
 
-const PackageList: React.FunctionComponent<PackageListProps> = ({ subjectId }) => {
+export const PackageList: React.FunctionComponent<PackageListProps> = ({ subjectId }) => {
     const router = useRouter();
+    const userStore = useStoreUser();
 
     const { pricePackageList } = useGetPricePackageListBySubjectId(subjectId);
     return (
@@ -38,13 +41,17 @@ const PackageList: React.FunctionComponent<PackageListProps> = ({ subjectId }) =
                                 </p>
                             </Link>
                         </div>
-                        <div>
-                            <Link href={router.asPath + routes.adminAddPackageUrl} passHref>
-                                <p className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm cursor-pointer hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
-                                    Add Packages
-                                </p>
-                            </Link>
-                        </div>
+                        {userStore.role.description === UserRole.ADMIN ? (
+                            <div>
+                                <Link href={router.asPath + routes.adminAddPackageUrl} passHref>
+                                    <p className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm cursor-pointer hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
+                                        Add Packages
+                                    </p>
+                                </Link>
+                            </div>
+                        ) : (
+                            <></>
+                        )}
                     </div>
                 </div>
             </div>
@@ -105,5 +112,3 @@ const PackageList: React.FunctionComponent<PackageListProps> = ({ subjectId }) =
         </div>
     );
 };
-
-export default PackageList;
