4e9b3eba5e72c9fb633579163978b37c17b3e365
BuildTools <kainesv86@gmail.com>
2022-08-01 22:50:09 +0700

    Fix auth quiz

--- src/packages/quiz/common/hooks/useGetQuizResultById.ts
+++ src/packages/quiz/common/hooks/useGetQuizResultById.ts
@@ -3,7 +3,7 @@ import { useGetDataById } from '../../../../core/common/hooks';
 import { QuizResult } from '../../../../core/models/quizResult';
 
 export const useGetQuizResultById = (quizId: string) => {
-    const { data: quiz } = useGetDataById<QuizResult>(ApiListRoutes.QUIZ_RESULT, quizId);
+    const { data: quiz, error } = useGetDataById<QuizResult>(ApiListRoutes.QUIZ_RESULT, quizId);
 
-    return { quiz };
+    return { quiz, error };
 };
